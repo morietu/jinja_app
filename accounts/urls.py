32035256272b5efbf57fd0f27e1aac316786c03f
morietu <ejb0515@gmail.com>
2025-08-18 16:42:18 +0900

    fix(auth): add missing name/parenthesis to login route

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 # accounts/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import RegisterView, mypage
+from .views import RegisterView, mypage, MyLoginView, MyLogoutView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/",  auth_views.LoginView.as_view(),  name="login"),   # ← 指定不要
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("login/",  MyLoginView.as_view(template_name="registration/login.html"),name="login"),
+    path("logout/", MyLogoutView.as_view(), name="logout"),
     path("mypage/", mypage, name="mypage"),
     path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),  # ← accounts/削除
 ]
