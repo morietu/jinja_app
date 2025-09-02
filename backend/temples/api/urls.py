17c96bf8b760e0074d81fb7354bc1cbe6fb27aa1
morietu <ejb0515@gmail.com>
2025-09-03 01:34:27 +0900

    feat(api): コンシェルジュAPIを整理・実装 - ConciergeRequestSerializer/HistorySerializer 整理 - /api/concierge POST で OpenAI を利用した神社推薦を返却 - /api/concierge/recommendations GET で候補神社を返す処理を追加

--- backend/temples/api/urls.py
+++ backend/temples/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from temples.api.views.concierge import ConciergeRecommendationsView
+
 from rest_framework.routers import DefaultRouter
 from temples.api.views.concierge import ConciergeAPIView, ConciergeHistoryListView
 from temples.api.views.geocode import GeocodeView
@@ -31,5 +33,6 @@ urlpatterns = [
     path("concierge/", ConciergeAPIView.as_view(), name="concierge"),
     path("concierge/history/", ConciergeHistoryListView.as_view(), name="concierge_history"),
     path("geocode/", GeocodeView.as_view(), name="geocode"),
+    path("concierge/recommendations/", ConciergeRecommendationsView.as_view(), name="concierge-recommendations"),
 ]
 
