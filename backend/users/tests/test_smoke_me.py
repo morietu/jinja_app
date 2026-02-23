8222213e139f2c08b869fbe0f01edf3f1ee5e7d4
morietsu <ejb0515@gmail.com>
2026-02-23 21:57:57 +0900

    tests: use namespaced users_api routes

--- backend/users/tests/test_smoke_me.py
+++ backend/users/tests/test_smoke_me.py
@@ -1,7 +1,6 @@
 import pytest
 from django.urls import reverse
 
-
 @pytest.mark.django_db
 def test_me_url_resolves():
-    assert reverse("users-me")
+    assert reverse("users_api:me") == "/api/users/me/"
