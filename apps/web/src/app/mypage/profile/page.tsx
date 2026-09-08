a0ee7d3ea21d371e7e20f4994cd6742c2b0dd348
Etsuko <ejb0515@gmail.com>
2026-09-08 10:31:13 +0900

    refactor: MVPからプロフィール専用ルートを除外

--- apps/web/src/app/mypage/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-// apps/web/src/app/mypage/profile/page.tsx
-import MyPageProfileView from "@/components/views/MyPageProfileView";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function MyPageProfilePage() {
-  return <MyPageProfileView />;
-}
